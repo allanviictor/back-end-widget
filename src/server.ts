86bdf154e74944b0ef335b2627c607ba65679de1
allan victor <allan_victor98@hotmail.com>
2022-05-08 19:44:58 -0300

    envio de email com nodemailer e mailtrap

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,27 @@
 import express from 'express';
 import { prisma } from './prisma'
+import nodemailer from "nodemailer";
 
 const app = express();
 
 app.use(express.json());
 
+var transport = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+        user: "19b08369eeff94",
+        pass: "bc4b61e9508c1d"
+    }
+});
+
+const linkTests = 'https://www.eusemfronteiras.com.br/wp-content/uploads/2018/09/42159296_s-810x538.jpg' 
+
 app.post('/feedbacks',async (req,res)=> {
 
     let {type, comment, screenShot} = req.body 
 
-    await prisma.feedback.create({
+    const feedback = await prisma.feedback.create({
         data: {
             type: type,
             comment: comment,
@@ -17,7 +29,20 @@ app.post('/feedbacks',async (req,res)=> {
         }
     })
 
-    return res.status(201).send({data: req.body})
+
+    await transport.sendMail({
+        from: "allan bananinha <bananinha@gmail.com>",
+        to: "allanzoka <allanviictor07@gmail.com>",
+        subject: 'feedback do aplicativo',
+        html: [
+            `<h2>tipo de feedback: ${type}</h2>`,
+            `<h2>comentario: ${comment}</h2>`,
+            `<h2>foto:</h2>`,
+            `<img style="width:500px;height:500px;object-fit:cover;" src=${screenShot}>`
+        ].join('\n')
+    })
+
+    return res.status(201).json({data: feedback})
 
 })
 
