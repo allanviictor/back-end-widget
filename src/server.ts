a3db8c667830173f488aa33789a3b08e3f46affc
allan victor <allan_victor98@hotmail.com>
2022-05-08 19:00:12 -0300

    primeira insercao no banco

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,28 @@
 import express from 'express';
+import { prisma } from './prisma'
 
+const app = express();
 
+app.use(express.json());
 
-express().listen(3000,()=> {
-    console.log('server is running !!')
+app.post('/feedbacks',async (req,res)=> {
+
+    let {type, comment, screenShot} = req.body 
+
+    await prisma.feedback.create({
+        data: {
+            type: type,
+            comment: comment,
+            screenShot: screenShot
+        }
+    })
+
+    return res.status(201).send({data: req.body})
+
 })
 
-const test = 1;
+
+
+app.listen(3000,()=> {
+    console.log('server is running !!')
+})
